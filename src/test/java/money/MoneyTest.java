c60a40e0714af0ff4ec136fe364184ce0c79a277
kuniaki.kawasaki <kuniaki.kawasaki@bizreach.co.jp>
2019-11-06 11:34:45 +0900

    三角測量

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -3,6 +3,8 @@ package money;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author kunipon
@@ -16,5 +18,11 @@ public class MoneyTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
